a42d4673b9d2741996f2059c07032adcc1710c5c
ShenBao <shenbaoone@gmail.com>
2017-08-18 21:47:10 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,11 @@ module.exports = cssnano(
         autoprefixer : {
             add      : true,
             remove   : true,
-            browsers : ['last 2 versions']
+            browsers : [
+                'last 2 versions',
+                'ios >= 8',
+                'Android >= 4.1'
+            ]
         },
         discardComments : {
             removeAll : true
--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Dev Boilerplate.!！!</h2>
+          <h2>Welcome to React Dev Boilerplate.!</h2>
         </div>
       </div>
     );
